eaa2e64fe997d00aaf62cb7707609aae82e05ce6
Connor177 <55096436+Connor177@users.noreply.github.com>
2019-09-29 13:43:47 -0500

    Update Form1.js
    added code for calculating monthly payments on a car loan

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -9,7 +9,19 @@ let user = prompt("What calculator do you want to use? (car or home or quit)")
 
 if (user == "car") {
 
-
+let loanAmountC = prompt("What is the loan amount?")
+let loanTimeC = prompt("What is the loan timeframe in months?")
+let loanRateC = prompt("What is the loan rate? ex. 5.7% = 5.7")
+//enter car loan function
+function carLoanPayment(amount, time, rate) {
+  let r = rate/100
+  let rMonth = r/12
+  let d = ((((1 + rMonth) ** time) - 1)/ ((rMonth * (1 + rMonth) ** time)))
+  return amount/d 
+  }
+  
+  let monthlyPayment = carLoanPayment(parseFloat(loanAmountC), parseFloat(loanTimeC),parseFloat(loanRateC))
+  alert(`A car loan for $${loanAmountC} over ${loanTimeC} months at ${loanRateC}% would have a monthly payment of $${Math.round(monthlyPayment)}.`)
 
 
 } else if (user == "home"){
