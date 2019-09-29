8355f9218dcc2a3e53693855665ebe71a4dde53f
Connor177 <55096436+Connor177@users.noreply.github.com>
2019-09-29 13:40:38 -0500

    Outline
    Outline for program

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,32 @@
+
+
+let n = 2
+for (n = 2; n > 0; n++) {
+
+
+let user = prompt("What calculator do you want to use? (car or home or quit)")
+
+
+if (user == "car") {
+
+
+
+
+} else if (user == "home"){
+let choice = Number(prompt("What mortgage term do you want? (15 or 30)"))
+if ((choice == 15) || (choice == 30)) {
+
+
+
+  
+} else {
+  alert("Oops, please choose 15 or 30")
+} 
+  
+} else if (user == "quit") {
+  alert("Have a nice day!")
+  break;
+} else {
+  }
+
+}
